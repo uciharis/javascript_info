517df1fbffeb9f4186e2eb3a8bf40eaf6bf2f4c5
uciharis <harisnop@gmail.com>
2024-10-12 20:58:20 +0700

    penjelasan ++var dan var++

--- 1f-basicOp.js
+++ 1f-basicOp.js
@@ -26,3 +26,100 @@ let s = 'my' + 'string';
 console.log(s); // output : mystring
 // nb : jika salah satu operand adalah string, maka operand lain dikonversi jd string
 console.log(1 + '9'); // output : 19
+console.log('1' + 9); // output : 19
+
+// jika operator '+' berusaha membuat operand menjadi string
+// tidak halnya dengan operator yang lain, seperti '-'
+
+console.log(6-'1'); // output: 5
+console.log('4'/'2'); // output: 2
+
+// ------------------------- //
+
+// unary +
+
+// unary + bisa konvert sesuatu jadi angka
+console.log(+true); // output : 1
+console.log(+''); // output : 0
+// kyk fungsi Number (wah shorthand nich)
+// hal ini bs dipakai untuk konvert string jadi number
+// kebanyakan hasil dari inputan form HTML defaultnya adalah string
+let nilai1 = '2';
+let nilai2 = '8';
+console.log(+nilai1 + +nilai2); // output: 10 (sesuai keinginan kita)
+console.log(Number(nilai1) + Number(nilai2)); // output: 10 (versi panjang dari unary +)
+
+// ------------------------- //
+
+// Prioritas operator
+
+/*
+(urutan teratas s.d terbawah)
+
+[ unary +, unary - ]
+[ ** ]
+[ *, / ]
+[ +, - ]
+ 
+*/
+
+// ------------------------- //
+
+// chaining assignment
+
+// kita dapat melakukan assignment secara berantai
+// contoh :
+let a,b,c;
+a=b=c= 99;
+console.log(a,b,c);
+
+// untuk keterbacaan kode, sbaiknya displit menjadi :
+let d = 212;
+a = d;
+b = d;
+c = d;
+console.log(a,b,c,d);
+
+// ------------------------- //
+
+
+// modif var ( timpa var)
+// contoh :
+let timpa = 39;
+timpa = timpa + 69;
+console.log(timpa); // kita ingin mengubah variabel tanpa menambah variabel baru (modif var yg exist)
+// misal kita buat var baru dan copy dari timpa
+let tambal = timpa;
+tambal += 690; // penulisan ini sama dengan tambal = tambal + 690
+console.log(tambal);
+// contoh lain :
+tambal -= 600;
+console.log(tambal);
+tambal *= 0.01;
+console.log(tambal);
+
+// ------------------------- //
+
+// increment/ decrement
+
+// ++ increase by 1
+// -- decrease by 1
+let counter = 4;
+counter ++;
+console.log(counter); // output : 5
+counter --;
+console.log(counter); // output : 4
+
+// nb : increment dan decrement hanya berlaku utk variabel, tidak utk value
+// nb : postfix form -> counter++
+// nb : prefix form -> ++counter
+// bedanya terlihat pada contoh berikut 
+
+// prefix
+let ticker = 1;
+let kicker = 1;
+let x = ++ticker;
+console.log('nilai pre-fix', x); // nilai x terupdate +1 menjadi 2
+let y = kicker++;
+console.log('nilai post-fix', y); // nilai y masih 1, justru kickernya yang 1+1
+console.log('nilai kicker', kicker); 
